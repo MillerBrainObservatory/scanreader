1e47e64991753c1167d19bb591ce0eb1ff872b55
FlynnOConnell <FlynnOConnell@gmail.com>
2024-07-16 11:56:38 -0400

    chore: fix setup

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-long_description = "Python TIFF Stack Reader for ScanImage 5 scans (including multiROI)."
+long_description = "Python TIFF Stack Reader for ScanImage recordings (including multiROI)."
 
 setup(
     name='scanreader',
     version='0.4.12',
     description="Reader for ScanImage 5 scans (including slow stacks and multiROI).",
     long_description=long_description,
-    author='Erick Cobos',
-    author_email='ecobos@bcm.edu',
     license='MIT',
-    url='https://github.com/atlab/scanreader',
-    keywords='ScanImage scanreader multiROI 2016b tiff',
+    url='https://github.com/MillerBrainObservtory/scanreader',
+    keywords='ScanImage scanreader multiROI 2019 tiff',
     packages=['scanreader'],
     install_requires=['numpy>=1.12.0', 'tifffile>=2019.2.22'],
     classifiers=[

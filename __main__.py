2ec5ab06b6b5e1d10c6bd70d45d354a40c771f01
FlynnOConnell <FlynnOConnell@gmail.com>
2024-07-24 12:17:57 -0400

    docs: add napari example

--- __main__.py
+++ __main__.py
@@ -3,28 +3,31 @@ __main__.py: scanreader entrypoint.
 """
 
 from pathlib import Path
-import matplotlib.pyplot as plt
+import zarr
+import napari
+from matplotlib import pyplot as plt
+
 import scanreader as sr
 
 
-def quick_save_image(arr, save_path):
-    fig, ax = plt.subplots()
-    ax.imshow(arr)
-    plt.savefig(save_path)
+def main(*args, **kwargs) -> sr.ScanLBM:
 
-
-def main():
-    # set up directories
-    datapath = Path().home() / "data"
-    rawpath = datapath / "raw"
+    # set the data path
+    if args:
+        datapath = Path(args[0])
+    else:
+        datapath = kwargs.get("datapath", Path().home() / "caiman_data")
 
     # read in the data
-    files = [
-        str(x) for x in rawpath.glob("*.tif*")
-    ]  # grab the first file in the directory
-    # return sr.read_scan(files, join_contiguous=True, lbm=True, x_cut=(0, 0), y_cut=(0, 0))
+    files = [str(x) for x in datapath.glob("*.tif*")]
     return sr.read_scan(files, join_contiguous=True)
 
 
 if __name__ == "__main__":
     scan = main()
+    data = scan[:, :, :, 0, :].squeeze()
+    viewer = napari.Viewer()
+    viewer.add_image(data, name="data", colormap='gray')
+    napari.run()
+
+x = 2

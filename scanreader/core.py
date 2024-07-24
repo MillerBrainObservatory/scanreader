3eecf1ed8eb578b1ad078ed67863731fbb0bbc9b
FlynnOConnell <FlynnOConnell@gmail.com>
2024-07-24 12:06:27 -0400

    feat: introduce ScanLBM with lazy operations

--- scanreader/core.py
+++ scanreader/core.py
@@ -1,6 +1,11 @@
+from __future__ import annotations
+
+import os
 import re
 from glob import glob
-from os import path
+from os import path, PathLike
+from pathlib import Path
+from typing import List, AnyStr
 
 import numpy as np
 from tifffile import TiffFile
@@ -18,37 +23,25 @@ _scans = {'5.1': scans.Scan5Point1, '5.2': scans.Scan5Point2, '5.3': scans.Scan5
           '2020': scans.Scan2020, '2021': scans.Scan2021}
 
 
-def read_scan(pathnames, dtype=np.int16, join_contiguous=True):
+def read_scan(pathnames: os.PathLike | List[os.PathLike | str], dtype=np.int16, join_contiguous=True):
     """
     Reads a ScanImage scan.
 
     Parameters
     ----------
-    pathnames: os.PathLike or str or list of os.PathLike or str
+    pathnames: os.PathLike
         Pathname(s) or pathname pattern(s) to read.
     dtype: data-type, optional
         Data type of the output array.
     join_contiguous: bool, optional
         For multiROI scans (2016b and beyond) it will join contiguous scanfields in the same depth.
         No effect in non-multiROI scans. See help of ScanMultiROI._join_contiguous_fields for details.
-    lbm: bool, optional
-        For Light Beads Microscopy datasets.
-    x_cut: slice-like, optional
-        Slice to cut in x_center_coordinate dimension.
-    y_cut: slice-like, optional
-        Slice to cut in y_center_coordinate dimension.
-
-    .. versionadded:: 0.1.0
-
-    .. note::
-        The `x_cut` and `y_cut` parameters are used to cut the scan in the x_center_coordinate and y_center_coordinate dimensions, respectively.
-        For example, `x_cut=slice(10, 20)` will start the image 10 pixels in, and end the image 20 pixels from the far edge.
 
     Returns
     -------
-
-    LBMScanMultiROI
-        A Scan object (subclass of BaseScan) with metadata and data. See Readme for details.
+    ScanLBM
+        A Scan object (subclass of ScanMultiROI) with metadata and different offset correction methods.
+        See Readme for details.
 
     """
     # Expand wildcards
@@ -65,7 +58,7 @@ def read_scan(pathnames, dtype=np.int16, join_contiguous=True):
     # Select the appropriate scan object
     if (version in ['2016b', '2017a', '2017b', '2018a', '2018b', '2019a', '2019b', '2020', '2021'] and
             is_scan_multiROI(file_info)):
-        scan = scans.ScanMultiROI(join_contiguous=join_contiguous)
+        scan = scans.ScanLBM(join_contiguous=join_contiguous)
     elif version in _scans:
         scan = _scans[version]()
     else:
@@ -78,26 +71,24 @@ def read_scan(pathnames, dtype=np.int16, join_contiguous=True):
     return scan
 
 
-def expand_wildcard(wildcard):
+def expand_wildcard(wildcard: os.PathLike | str | list[os.PathLike | str]) -> list[PathLike[AnyStr]]:
     """ Expands a list of pathname patterns to form a sorted list of absolute filenames. """
-    if isinstance(wildcard, str):
+    if hasattr(wildcard, 'suffix'):
+        wildcard_list = [str(wildcard)]
+    elif isinstance(wildcard, str):
         wildcard_list = [wildcard]
     elif isinstance(wildcard, (tuple, list)):
         wildcard_list = wildcard
     else:
-        error_msg = 'Expected string or list of strings, received {}'.format(wildcard)
+        error_msg = f'Expected string or list of strings, received: {wildcard}'
         raise TypeError(error_msg)
 
     # Expand wildcards
     rel_filenames = [glob(wildcard) for wildcard in wildcard_list]
     rel_filenames = [item for sublist in rel_filenames for item in sublist]  # flatten list
 
-    # Make absolute filenames
     abs_filenames = [path.abspath(filename) for filename in rel_filenames]
-
-    # Sort
     sorted_filenames = sorted(abs_filenames, key=path.basename)
-
     return sorted_filenames
 
 

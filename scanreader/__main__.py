39fd72a4c185013621489dd23ccfa920e2d1ec4b
Flynn OConnell <FlynnOConnell@gmail.com>
2024-08-19 23:54:19 -0400

    feat: debug flag, quickplot

--- scanreader/__main__.py
+++ scanreader/__main__.py
@@ -7,13 +7,12 @@ from pathlib import Path
 import argparse
 
 import dask
-import numpy as np
 
 import scanreader as sr
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Read a LBM ScanImage .tiff file.")
+    parser = argparse.ArgumentParser(description="scanreader CLI. Read a LBM ScanImage .tiff file.")
     help_str = (
         "Path to the .tiff file or directory containing the .tiff files. \n"
         "If a directory is given, all .tiff files in the directory will be read. \n"
@@ -31,6 +30,11 @@ def parse_args():
     )
     parser.add_argument(
         "-y", "--yslice", type=str, help="Y-pixels to read (i.e. 1:50)", default=":"
+    )
+    parser.add_argument(
+        "-d", "--debug", type=str,
+        help="Flag (True/False) to enable debug logs to the terminal (to file coming soon).",
+        default="False"
     )
     _args = parser.parse_args()
     if _args.path is None:
@@ -56,37 +60,20 @@ def process_slice_str(slice_str):
 def process_slice_objects(slice_str):
     return tuple(map(process_slice_str, slice_str.split(",")))
 
-
-def trim(_init_scan, amounts_x):
-    new_slice_x = [
-        slice(s.start + amounts_x[0], s.stop - amounts_x[1])
-        for s in _init_scan.fields[0].output_xslices
-    ]
-    return [i for s in new_slice_x for i in range(s.start, s.stop)]
-
-
-# needed as entrypoint to napari
+# entrypoint to napari
 def imread(path, slice_objects: typing.Iterable) -> dask.core.Any:
-    __scan = sr.read_scan(path, join_contiguous=True, debug=True)
+    __scan = sr.read_scan(path, debug=True)
     return __scan[slice_objects]
 
+def quickplot(array):
+    import matplotlib.pyplot as plt
+    plt.imshow(array, cmap='gray')
+    plt.show()
 
 if __name__ == "__main__":
 
     args = parse_args()
-    _scan = sr.read_scan(args.path, join_contiguous=True)
-    for i in range(0, _scan.shape[1] - 1):
-        scan = _scan[:, i, :, :]
-
-        # Assuming scan is a 3D array with shape (Time, Y, X)
-        threshold = 1e-5
-
-        # Identify non-zero rows (across the X axis)
-        non_zero_rows = ~np.all(np.abs(scan) < threshold, axis=(0, 2))
-
-        # Identify non-zero columns (across the Y axis)
-        non_zero_cols = ~np.all(np.abs(scan) < threshold, axis=(0, 1))
-
-        # Slice the array to remove the zero or near-zero rows/columns
-        item_cleaned = scan[:, non_zero_rows, :]
-        item_cleaned = item_cleaned[:, :, non_zero_cols]
+    _scan = sr.read_scan(args.path,)
+    _scan.trim_x = (8,8)
+    quickplot(_scan[2,0,:,:])
+    x=2

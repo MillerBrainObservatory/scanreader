5952147650e128231650b02c2d09d46e8a24b66f
Flynn OConnell <FlynnOConnell@gmail.com>
2024-11-07 14:44:02 -0500

    feat: print params

--- scanreader/__main__.py
+++ scanreader/__main__.py
@@ -5,9 +5,11 @@ __main__.py: scanreader entrypoint.
 import os
 import argparse
 import logging
+from pathlib import Path
 import scanreader as sr
 from scanreader.scans import get_metadata
 from scanreader import get_files, get_single_file
+import tifffile
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -20,27 +22,41 @@ else:
     logger.setLevel(logging.INFO)
 
 
+def print_params(params, indent=5):
+    for k, v in params.items():
+        # if value is a dictionary, recursively call the function
+        if isinstance(v, dict):
+            print(" " * indent + f"{k}:")
+            print_params(v, indent + 4)
+        else:
+            print(" " * indent + f"{k}: {v}")
+
 def main():
     parser = argparse.ArgumentParser(description="Scanreader CLI for processing ScanImage tiff files.")
     parser.add_argument("path", type=str, nargs="?", default=None,
                         help="Path to the file or directory to process.")
-    parser.add_argument("-f", "--frames", type=str, default=":",
+    parser.add_argument("-t", "--frames", type=str, default=":",
                         help="Frames to read. Use slice notation like NumPy arrays (e.g., 1:50, 10:100:2).")
-    parser.add_argument("-m", "--metadata", action="store_true",
-                        help="Print a dictionary of metadata.")
     parser.add_argument("-z", "--zplanes", type=str, default=":",
                         help="Z-Planes to read. Use slice notation like NumPy arrays (e.g., 1:50, 5:15:2).")
-    parser.add_argument("-x", "--xslice", type=str, default=":",
-                        help="X-pixels to read. Use slice notation like NumPy arrays (e.g., 100:500, 0:200:5).")
-    parser.add_argument("-y", "--yslice", type=str, default=":",
-                        help="Y-pixels to read. Use slice notation like NumPy arrays (e.g., 100:500, 50:250:10).")
-    parser.add_argument("-tx", "--trim_x", type=int, nargs=2, default=(0, 0),
+    parser.add_argument("-x", "--trim_x", type=int, nargs=2, default=(0, 0),
                         help="Number of x-pixels to trim from each ROI. Tuple or list (e.g., 4 4 for left and right edges).")
-    parser.add_argument("-ty", "--trim_y", type=int, nargs=2, default=(0, 0),
+    parser.add_argument("-y", "--trim_y", type=int, nargs=2, default=(0, 0),
                         help="Number of y-pixels to trim from each ROI. Tuple or list (e.g., 4 4 for top and bottom edges).")
-    parser.add_argument("-d", "--debug", action="store_true",
-                        help="Enable debug logs to the terminal.")
-    
+    # Boolean Flags
+    parser.add_argument("-m", "--metadata", action="store_true",
+                        help="Print a dictionary of metadata.")
+    parser.add_argument( "--volume",
+                         action='store_true',
+                         help="Save the data as a 3D volumetric recording"
+                         )
+    parser.add_argument( "--roi",
+                         action='store_true',
+                         help="Save each ROI in its own folder"
+                         )
+    # Commands
+    parser.add_argument("--extract", type=str, help="Extract data to designated filetype")
+
     args = parser.parse_args()
 
     if not args.path:
@@ -57,21 +73,37 @@ def main():
 
     if args.metadata:
         metadata = get_metadata(files[0])
+        # filter out the verbose scanimage frame/roi metadata
+        print_params({k: v for k, v in metadata.items() if k not in ['si', 'roi_info']})
+
+    if args.extract:
+        frames = process_slice_str(args.frames)
+        zplanes = process_slice_str(args.zplanes)
+
+        scan = sr.ScanLBM(
+            files,
+            trim_roi_x=args.trim_x,
+            trim_roi_y=args.trim_y,
+        )
+        # --volume
+        # --roi
+        if args.volume:
+            data = scan[:]
+        elif args.roi:
+            for roi in scan.yslices:
+                data = scan[:, :, roi, :]
+        else:
+            savepath = Path(args.extract).expanduser()
+            print(f'Saving z-planes to {savepath}.')
+            for plane in range(0, scan.num_planes):
+                data = scan[:, plane, :, :]
+                name = savepath / f'assembled_plane_{plane + 1}.tif'
+                tifffile.imwrite(name, data)
+
+
+        return scan
+    else:
         return metadata
-
-    frames = process_slice_str(args.frames)
-    zplanes = process_slice_str(args.zplanes)
-    xslice = process_slice_str(args.xslice)
-    yslice = process_slice_str(args.yslice)
-
-    scan = sr.ScanLBM(
-        files,
-        trim_roi_x=args.trim_x,
-        trim_roi_y=args.trim_y,
-        debug=args.debug,
-        save_path=os.path.join(args.path, 'zarr'),
-    )
-    return scan
 
 
 
